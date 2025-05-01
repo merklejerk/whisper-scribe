64b1b704bb2bedcf24f76cd5ea933ce8fa29b03b
Lawrence Forman <me@merklejerk.com>
2025-05-01 01:35:45 -0400

    tweaks

--- src/transcriber.py
+++ src/transcriber.py
@@ -130,7 +130,7 @@ class Transcriber(Generic[MetadataT]):
             generate_kwargs={
                 "language": "english",
                 "task": "transcribe",
-                "temperature": (0.0, 0.2, 0.4),
+                "temperature": (0.0, 0.1, 0.2, 0.4),
                 "forced_decoder_ids": None,
                 "logprob_threshold": -1.0,
             },
